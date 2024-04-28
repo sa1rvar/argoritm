1bb91f0bdfb5ede148388e6d5b0ebe966934169e
Sarvar <sabdumutolibov@gmail.com>
2024-04-28 11:11:57 +0500

    'task'

---
+++ main.py
@@ -0,0 +1,40 @@
+# #TASK1
+# def get_first_value (lst):
+#     return lst[0]
+#
+# print(get_first_value([1, 2, 3]))
+# print(get_first_value([80, 5, 100]))
+# print(get_first_value([-500, 0, 50]))
+#
+# #TASK2
+# def concat (lst1, lst2):
+#     result = lst1 + lst2
+#     return result
+#
+# print(concat([1, 3, 5] [2, 6, 8]))
+# print(concat([7, 8] [10, 9, 1, 1, 2]))
+# print(concat([4, 5, 1] [3, 3, 3, 3, 4]))
+#
+# #TASK3
+# def get_last_item (lst):
+#     return lst[-1]
+# print(get_last_item([1, 2, 3,]))
+# print(get_last_item(["cat", "dog", "duck"]))
+# print(get_last_item([True, False, True]))
+# print(get_last_item([7, "String", False]))
+#
+# #TASK4
+# def make_pair (x, y):
+#     return x, y
+#
+# print(make_pair(1, 2))
+# print(make_pair(51, 21))
+# print(make_pair(512124, 215))
+#
+
+
+
+
+
+
+
